75da4b21df8f8d345edef21c9ebb26fb1de74217
sebben34 <sebastien@sospr.fr>
2026-07-07 23:43:25 +0200

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'dany-capture-v01';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/db.js',
+  './js/ui.js',
+  './js/app.js',
+  './manifest.json',
+  './images/icon.svg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(key) {
+        return key !== CACHE_NAME;
+      }).map(function(key) {
+        return caches.delete(key);
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      return cached || fetch(event.request);
+    })
+  );
+});
